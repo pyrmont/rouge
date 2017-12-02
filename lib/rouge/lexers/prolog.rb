ed8173d32f4e54f46adbfb5990b373fe8ab1fad3
Darius Foo <darius.foo.tw@gmail.com>
2017-12-02 11:37:26 +0800

    Highlight #! lines in Prolog sources

--- lib/rouge/lexers/prolog.rb
+++ lib/rouge/lexers/prolog.rb
@@ -12,7 +12,7 @@ module Rouge
 
       state :basic do
         rule /\s+/, Text
-        rule /^#.*/, Comment::Single
+        rule /^#!.*/, Comment::Single
         rule /%.*/, Comment::Single
         rule /\/\*/, Comment::Multiline, :nested_comment
 
